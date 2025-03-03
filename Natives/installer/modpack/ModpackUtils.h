d9598b70998da79afa48b4740f7cebdfcc5e3efb
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-03 16:55:49 -0600

    Update ModpackUtils.h

--- Natives/installer/modpack/ModpackUtils.h
+++ Natives/installer/modpack/ModpackUtils.h
@@ -5,6 +5,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ModpackUtils : NSObject
 + (void)archive:(UZKArchive *)archive extractDirectory:(NSString *)dir toPath:(NSString *)path error:(NSError **)error;
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency;
++ (NSDictionary *)parseVersionString:(NSString *)versionString;
 @end
 
 NS_ASSUME_NONNULL_END
