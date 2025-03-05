eb7221810cca48d629595231f9f9746c97353ea0
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 16:01:14 -0600

    Update FabricUtils.h

--- Natives/installer/FabricUtils.h
+++ Natives/installer/FabricUtils.h
@@ -1,7 +1,18 @@
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * Utility class for Fabric/Quilt mod loader installation
+ */
 @interface FabricUtils : NSObject
 
+/**
+ * Returns endpoints configuration for Fabric and Quilt services
+ * @return Dictionary with endpoint configurations
+ */
 + (NSDictionary *)endpoints;
 
 @end
+
+NS_ASSUME_NONNULL_END
