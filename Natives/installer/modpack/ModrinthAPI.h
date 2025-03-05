d97b71edfb0d825e8fd5eb812949f2e970c948e5
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 16:04:44 -0600

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -1,14 +1,43 @@
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * Modrinth API implementation for accessing mods and modpacks from Modrinth
+ */
 @interface ModrinthAPI : ModpackAPI
 
-- (NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, id> *)searchFilters previousPageResult:(NSMutableArray *)modrinthSearchResult;
+/**
+ * Searches for mods or modpacks on Modrinth
+ * @param searchFilters Dictionary of search filters
+ * @param modrinthSearchResult Previous search results for pagination
+ * @return Array of search results
+ */
+- (nullable NSMutableArray *)searchModWithFilters:(NSDictionary<NSString *, id> *)searchFilters 
+                                previousPageResult:(nullable NSMutableArray *)modrinthSearchResult;
+
+/**
+ * Synchronously loads details for a mod or modpack
+ * @param item The mod or modpack to load details for
+ */
 - (void)loadDetailsOfModSync:(NSMutableDictionary *)item;
-- (void)loadDetailsOfMod:(NSMutableDictionary *)item completion:(void (^)(NSError *error))completion;
-- (void)loadDetailsOfMod:(NSMutableDictionary *)item;
-- (void)installModFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
+
+/**
+ * Asynchronously loads details for a mod or modpack
+ * @param item The mod or modpack to load details for
+ * @param completion Block to call when the operation completes
+ */
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item 
+              completion:(void (^)(NSError * _Nullable error))completion;
+
+/**
+ * Installs a mod from the provided detail at the selected version
+ * @param modDetail The mod details
+ * @param selectedVersion The index of the selected version
+ */
+- (void)installModFromDetail:(NSDictionary *)modDetail 
+                     atIndex:(NSUInteger)selectedVersion;
 
 @end
 
