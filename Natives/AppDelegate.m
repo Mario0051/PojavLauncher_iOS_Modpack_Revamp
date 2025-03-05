95cd4f6da5140aa1a4e690312035746d47a3405f
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 12:38:29 -0600

    Update AppDelegate.m

--- Natives/AppDelegate.m
+++ Natives/AppDelegate.m
@@ -1,34 +1,66 @@
 #import "AppDelegate.h"
-#import "SceneDelegate.h"
-#import "ios_uikit_bridge.h"
-#import "utils.h"
-
-// SurfaceViewController
-extern dispatch_group_t fatalExitGroup;
+#import <Foundation/Foundation.h>
 
 @implementation AppDelegate
 
-#pragma mark - UISceneSession lifecycle
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Initialize file manager and Documents directory path
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *documentsDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
 
+    // Define paths for .pojavlauncher (accounts) and instance directories
+    NSString *pojavDir = [documentsDir stringByAppendingPathComponent:@".pojavlauncher"];
+    NSString *accountsDir = [pojavDir stringByAppendingPathComponent:@"accounts"];
+    NSString *vanillaDir = [documentsDir stringByAppendingPathComponent:@"minecraft_vanilla"];
+    NSString *fabricDir = [documentsDir stringByAppendingPathComponent:@"minecraft_fabric"];
+    NSString *forgeDir = [documentsDir stringByAppendingPathComponent:@"minecraft_forge"];
+    NSString *quiltDir = [documentsDir stringByAppendingPathComponent:@"minecraft_quilt"];
+    NSString *neoForgeDir = [documentsDir stringByAppendingPathComponent:@"minecraft_neoforge"];
 
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    if (fatalExitGroup != nil) {
-        dispatch_group_leave(fatalExitGroup);
-        fatalExitGroup = nil;
+    NSError *error = nil;
+    // Ensure .pojavlauncher/accounts directory exists (for account JSON files)
+    if (![fileManager fileExistsAtPath:accountsDir]) {
+        if (![fileManager createDirectoryAtPath:accountsDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating accounts directory: %@", error);
+        }
+        error = nil;
     }
+    // Ensure each instance directory exists (Vanilla, Fabric, Forge, Quilt, NeoForge)
+    if (![fileManager fileExistsAtPath:vanillaDir]) {
+        if (![fileManager createDirectoryAtPath:vanillaDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating Vanilla instance directory: %@", error);
+        }
+        error = nil;
+    }
+    if (![fileManager fileExistsAtPath:fabricDir]) {
+        if (![fileManager createDirectoryAtPath:fabricDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating Fabric instance directory: %@", error);
+        }
+        error = nil;
+    }
+    if (![fileManager fileExistsAtPath:forgeDir]) {
+        if (![fileManager createDirectoryAtPath:forgeDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating Forge instance directory: %@", error);
+        }
+        error = nil;
+    }
+    if (![fileManager fileExistsAtPath:quiltDir]) {
+        if (![fileManager createDirectoryAtPath:quiltDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating Quilt instance directory: %@", error);
+        }
+        error = nil;
+    }
+    if (![fileManager fileExistsAtPath:neoForgeDir]) {
+        if (![fileManager createDirectoryAtPath:neoForgeDir withIntermediateDirectories:YES attributes:nil error:&error]) {
+            NSLog(@"Error creating NeoForge instance directory: %@", error);
+        }
+        error = nil;
+    }
+
+    //Log directory setup success
+    NSLog(@"Initialized instance directories at %@", documentsDir);
+
+    return YES;
 }
 
 @end
