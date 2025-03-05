dc2fb4baa046193cb5f52edb990827d5b3b7ea50
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 16:05:52 -0600

    Update ModMenuViewController.h

--- Natives/installer/ModMenuViewController.h
+++ Natives/installer/ModMenuViewController.h
@@ -1,4 +1,34 @@
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * View controller for browsing and installing mods from repositories
+ */
 @interface ModMenuViewController : UITableViewController <UISearchResultsUpdating>
+
+/**
+ * Updates the mod list based on current search filters
+ */
+- (void)updateModsList;
+
+/**
+ * Shows the mod installation queue
+ */
+- (void)actionShowQueue;
+
+/**
+ * Presents a profile selection dialog
+ */
+- (void)actionChooseProfile;
+
+/**
+ * Handles the installation of a mod
+ * @param mod The mod to install
+ * @param index The version index to install
+ */
+- (void)installModNow:(NSDictionary *)mod versionIndex:(NSUInteger)index;
+
 @end
+
+NS_ASSUME_NONNULL_END
