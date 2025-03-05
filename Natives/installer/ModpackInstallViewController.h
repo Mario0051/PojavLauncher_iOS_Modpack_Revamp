2fc1eeb372ae4fd28610b2f1f2e15d2813450475
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 17:21:14 -0600

    Update ModpackInstallViewController.h

--- Natives/installer/ModpackInstallViewController.h
+++ Natives/installer/ModpackInstallViewController.h
@@ -1,4 +1,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ModpackInstallViewController : UITableViewController<UISearchResultsUpdating>
+
+/**
+ * Handles the installation of a modpack
+ * @param details The modpack details
+ * @param index The version index to install
+ */
+- (void)installModpackFromDetail:(NSDictionary *)details atIndex:(NSUInteger)index;
+
 @end
