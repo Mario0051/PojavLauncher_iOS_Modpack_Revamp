d6ef1e09eb1b97fdffdb0ad888a18dc8df262f40
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 14:56:53 -0600

    Update ModrinthAPI.m

--- Natives/installer/modpack/ModrinthAPI.m
+++ Natives/installer/modpack/ModrinthAPI.m
@@ -168,4 +168,127 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:@"InstallMod" object:self userInfo:userInfo];
 }
 
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath {
+    NSError *extractError = nil;
+    UZKArchive *archive = [[UZKArchive alloc] initWithPath:packagePath error:&extractError];
+    if (extractError) {
+        NSLog(@"[ModrinthAPI] Error opening package: %@", extractError);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to open modpack: %@", extractError.localizedDescription]];
+        });
+        return;
+    }
+    
+    // Extract the modpack
+    NSError *error = nil;
+    BOOL extractionSuccess = YES;
+    
+    [archive performOnFilesInArchive:^(UZKFileInfo *fileInfo, BOOL *stop) {
+        NSString *destItemPath = [destPath stringByAppendingPathComponent:fileInfo.filename];
+        if (fileInfo.isDirectory) {
+            NSError *dirError = nil;
+            BOOL created = [[NSFileManager defaultManager] createDirectoryAtPath:destItemPath 
+                                                     withIntermediateDirectories:YES 
+                                                                      attributes:nil 
+                                                                           error:&dirError];
+            if (!created || dirError) {
+                NSLog(@"[ModrinthAPI] Extraction error for directory %@: %@", destItemPath, dirError);
+                *stop = YES;
+                extractionSuccess = NO;
+            }
+        } else {
+            NSError *fileError = nil;
+            NSData *data = [archive extractData:fileInfo error:&fileError];
+            if (!data || fileError) {
+                NSLog(@"[ModrinthAPI] Extraction error for file %@: %@", fileInfo.filename, fileError);
+                *stop = YES;
+                extractionSuccess = NO;
+            } else {
+                BOOL written = [data writeToFile:destItemPath options:NSDataWritingAtomic error:&fileError];
+                if (!written || fileError) {
+                    NSLog(@"[ModrinthAPI] Write error for file %@: %@", destItemPath, fileError);
+                    *stop = YES;
+                    extractionSuccess = NO;
+                }
+            }
+        }
+    } error:&error];
+    
+    if (!extractionSuccess || error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Extraction failed: %@", error.localizedDescription]];
+        });
+        return;
+    }
+    
+    // Look for the modpack metadata (modrinth.index.json)
+    NSString *modpackMetadataPath = [destPath stringByAppendingPathComponent:@"modrinth.index.json"];
+    NSData *metadataData = [NSData dataWithContentsOfFile:modpackMetadataPath];
+    if (!metadataData) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [downloader finishDownloadWithErrorString:@"Missing modpack metadata (modrinth.index.json)"];
+        });
+        return;
+    }
+    
+    NSError *jsonError = nil;
+    NSDictionary *metadata = [NSJSONSerialization JSONObjectWithData:metadataData options:0 error:&jsonError];
+    if (jsonError) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to parse modpack metadata: %@", jsonError.localizedDescription]];
+        });
+        return;
+    }
+    
+    // Process the modpack
+    NSString *profileName = metadata[@"name"] ?: destPath.lastPathComponent;
+    NSString *gameDir = [ModpackUtils getUniqueProfileDirectory:profileName];
+    
+    [ModpackUtils createProfileDirectory:gameDir];
+    
+    // Extract Minecraft version and mod loader version from the metadata
+    NSString *minecraftVersion = metadata[@"dependencies"][@"minecraft"];
+    NSString *modLoader = @"";
+    NSString *modLoaderVersion = @"";
+    if (metadata[@"dependencies"][@"fabric-loader"]) {
+        modLoader = @"fabric";
+        modLoaderVersion = metadata[@"dependencies"][@"fabric-loader"];
+    } else if (metadata[@"dependencies"][@"forge"]) {
+        modLoader = @"forge";
+        modLoaderVersion = metadata[@"dependencies"][@"forge"];
+    } else if (metadata[@"dependencies"][@"quilt-loader"]) {
+        modLoader = @"quilt";
+        modLoaderVersion = metadata[@"dependencies"][@"quilt-loader"];
+    }
+    
+    NSString *finalVersionString = @"";
+    if ([modLoader isEqualToString:@"fabric"]) {
+        finalVersionString = [NSString stringWithFormat:@"fabric-loader-%@-%@", modLoaderVersion, minecraftVersion];
+    } else if ([modLoader isEqualToString:@"forge"]) {
+        finalVersionString = [NSString stringWithFormat:@"%@-forge-%@", minecraftVersion, modLoaderVersion];
+    } else if ([modLoader isEqualToString:@"quilt"]) {
+        finalVersionString = [NSString stringWithFormat:@"quilt-loader-%@-%@", modLoaderVersion, minecraftVersion];
+    } else {
+        finalVersionString = minecraftVersion;
+    }
+    
+    // Create the profile
+    NSDictionary *profileInfo = @{
+        @"gameDir": gameDir,
+        @"name": profileName,
+        @"lastVersionId": finalVersionString,
+        @"icon": @""
+    };
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"[ModrinthAPI] Setting profile: %@", profileName);
+        PLProfiles.current.profiles[profileName] = [profileInfo mutableCopy];
+        PLProfiles.current.selectedProfileName = profileName;
+        [PLProfiles.current save];
+        
+        // Installation succeeded
+        [downloader.progress setCompletedUnitCount:downloader.progress.totalUnitCount];
+    });
+}
+
 @end
