97220741dea844d1d599be916d90cbe78c38c37a
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 16:02:30 -0600

    Update ForgeInstallViewController.h

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -1,4 +1,37 @@
 #import <UIKit/UIKit.h>
 
-@interface ForgeInstallViewController : UITableViewController
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * View controller for installing Forge and NeoForge mod loaders
+ */
+@interface ForgeInstallViewController : UITableViewController <NSXMLParserDelegate>
+
+/**
+ * Handles user selection of a Forge version
+ */
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+
+/**
+ * Handles tap on section headers to expand/collapse them
+ */
+- (void)tableViewDidSelectSection:(UITapGestureRecognizer *)sender;
+
+/**
+ * Changes between Forge and NeoForge repositories
+ */
+- (void)segmentChanged:(UISegmentedControl *)segment;
+
+/**
+ * Cancels ongoing downloads
+ */
+- (void)actionCancelDownload;
+
+/**
+ * Closes the installation view
+ */
+- (void)actionClose;
+
 @end
+
+NS_ASSUME_NONNULL_END
