b4b31e268cc2e6f885d71f2388e7a743c2d80d44
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 16:00:37 -0600

    Update FabricInstallViewController.h

--- Natives/installer/FabricInstallViewController.h
+++ Natives/installer/FabricInstallViewController.h
@@ -2,4 +2,20 @@
 #import "PLPrefTableViewController.h"
 
 @interface FabricInstallViewController : PLPrefTableViewController
+
+/**
+ * Initializes and configures the view controller for Fabric/Quilt installation
+ */
+- (void)viewDidLoad;
+
+/**
+ * Closes the installation view
+ */
+- (void)actionClose;
+
+/**
+ * Performs the Fabric/Quilt installation
+ */
+- (void)actionDone:(UIBarButtonItem *)sender;
+
 @end
