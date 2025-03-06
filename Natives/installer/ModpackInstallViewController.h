ee6098bbc3bf27170e05902cb8d540f0da166705
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-05 20:04:31 -0600

    Update ModpackInstallViewController.h

--- Natives/installer/ModpackInstallViewController.h
+++ Natives/installer/ModpackInstallViewController.h
@@ -1,12 +1,22 @@
 #import <UIKit/UIKit.h>
 
-@interface ModpackInstallViewController : UITableViewController<UISearchResultsUpdating>
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * View controller for browsing and installing modpacks from Modrinth and CurseForge
+ */
+@interface ModpackInstallViewController : UITableViewController <UISearchResultsUpdating>
 
 /**
  * Handles the installation of a modpack
- * @param details The modpack details
+ * @param details The modpack details dictionary
  * @param index The version index to install
+ * @param source The source of the modpack (0 = Modrinth, 1 = CurseForge)
  */
-- (void)installModpackFromDetail:(NSDictionary *)details atIndex:(NSUInteger)index;
+- (void)installModpackWithDetails:(NSDictionary *)details 
+                          atIndex:(NSUInteger)index 
+                           source:(NSInteger)source;
 
 @end
+
+NS_ASSUME_NONNULL_END
