48f229acd264420dd9ed3277429cdc79ff608a8f
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-07 08:28:54 -0600

    Update MinecraftResourceDownloadTask.h

--- Natives/MinecraftResourceDownloadTask.h
+++ Natives/MinecraftResourceDownloadTask.h
@@ -2,7 +2,14 @@
 
 @class ModpackAPI;
 
+typedef NS_ENUM(NSInteger, DownloadSource) {
+    DownloadSourceMinecraft,
+    DownloadSourceCurseForge,
+    DownloadSourceModrinth
+};
+
 @interface MinecraftResourceDownloadTask : NSObject
+
 @property NSProgress *progress, *textProgress;
 @property NSMutableArray *fileList, *progressList;
 @property NSMutableDictionary* metadata;
@@ -22,7 +29,11 @@
  * @param path The destination path
  * @return The created download task
  */
-- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path;
+- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url 
+                                            size:(NSUInteger)size 
+                                             sha:(NSString *)sha 
+                                         altName:(NSString *)altName 
+                                          toPath:(NSString *)path;
 
 /**
  * Creates a new download task with completion callback
@@ -34,7 +45,12 @@
  * @param success The callback to execute when download completes successfully
  * @return The created download task
  */
-- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url size:(NSUInteger)size sha:(NSString *)sha altName:(NSString *)altName toPath:(NSString *)path success:(void (^)(void))success;
+- (NSURLSessionDownloadTask *)createDownloadTask:(NSString *)url 
+                                            size:(NSUInteger)size 
+                                             sha:(NSString *)sha 
+                                         altName:(NSString *)altName 
+                                          toPath:(NSString *)path 
+                                         success:(void (^)())success;
 
 /**
  * Finishes the download process with an error message
@@ -54,6 +70,36 @@
  * @param modDetail The modpack details
  * @param selectedVersion The index of the selected version
  */
-- (void)downloadModpackFromAPI:(ModpackAPI *)api detail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion;
+- (void)downloadModpackFromAPI:(ModpackAPI *)api 
+                        detail:(NSDictionary *)modDetail 
+                       atIndex:(NSUInteger)selectedVersion;
+
+/**
+ * Retrieves metadata for a specific download task
+ * @param task The download task to retrieve metadata for
+ * @return A dictionary containing task metadata
+ */
+- (NSDictionary *)getDownloadTaskInfo:(NSURLSessionDownloadTask *)task;
+
+/**
+ * Retrieves metadata for all downloaded files
+ * @return An array of dictionaries containing download task metadata
+ */
+- (NSArray<NSDictionary *> *)getAllDownloadedFiles;
+
+/**
+ * Logs the current download source
+ * @param source The download source to log
+ */
+- (void)logDownloadSource:(DownloadSource)source;
+
+/**
+ * Checks the SHA of a downloaded file
+ * @param sha The expected SHA hash
+ * @param path The path to the file
+ * @param altName An alternate name for logging
+ * @return YES if the file passes SHA verification, NO otherwise
+ */
+- (BOOL)checkSHA:(NSString *)sha forFile:(NSString *)path altName:(NSString *)altName;
 
 @end
