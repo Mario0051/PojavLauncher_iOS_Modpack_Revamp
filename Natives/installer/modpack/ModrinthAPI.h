45c24d173738dc82baa3cb1455891c2c11a78a49
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-07 12:22:03 -0600

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "ModpackAPI.h"
+#import "UnzipKit.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -49,6 +50,40 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSArray *)filterVersionsForGameVersion:(nullable NSString *)gameVersion 
                                    loader:(nullable NSString *)loader 
                              fromVersions:(NSArray *)versions;
+
+/**
+ * Extracts overrides from a modpack archive with progress reporting
+ * @param archive The archive to extract from
+ * @param destPath The destination path
+ * @param progressCallback Block to call with progress updates (0.0 - 1.0)
+ * @param error Pointer to an error object that will be populated on failure
+ */
+- (void)extractOverrides:(UZKArchive *)archive 
+                  toPath:(NSString *)destPath 
+            withProgress:(void (^)(double progress))progressCallback 
+                   error:(NSError **)error;
+
+/**
+ * Downloads mod files from a modpack manifest
+ * @param files Array of files to download
+ * @param destPath Destination path for downloaded files
+ * @param downloader The download task handler
+ * @param completion Block to call when all downloads are complete
+ */
+- (void)downloadModFiles:(NSArray *)files 
+               toDestPath:(NSString *)destPath 
+           withDownloader:(MinecraftResourceDownloadTask *)downloader 
+            andCompletion:(void (^)(void))completion;
+
+/**
+ * Finalizes modpack installation by setting up profile and completing progress
+ * @param indexDict The modpack index dictionary
+ * @param destPath The destination path
+ * @param downloader The download task handler
+ */
+- (void)finalizeModpackInstallation:(NSDictionary *)indexDict 
+                           destPath:(NSString *)destPath 
+                         downloader:(MinecraftResourceDownloadTask *)downloader;
 
 @end
 
