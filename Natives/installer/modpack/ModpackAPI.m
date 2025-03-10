c52f5e15902f6bba0b9d3e1af7fd2457d7cf41ef
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-09 19:03:27 -0500

    Update ModpackAPI.m

--- Natives/installer/modpack/ModpackAPI.m
+++ Natives/installer/modpack/ModpackAPI.m
@@ -1,39 +1,77 @@
-#import "AFNetworking.h"
 #import "MinecraftResourceDownloadTask.h"
 #import "ModpackAPI.h"
+#import "PLProfiles.h"
+#import "ModpackUtils.h"
+#import "UnzipKit.h"
+#import "AFNetworking.h"
 #import "utils.h"
+
+@interface ModpackAPI ()
+@property(nonatomic, strong) NSOperationQueue *operationQueue;
+@property(nonatomic, strong) NetworkService *networkService;
+@end
 
 @implementation ModpackAPI
 
 #pragma mark - Initialization
 
 - (instancetype)initWithURL:(NSString *)url {
-    if (self = [super init]) {
+    self = [super init];
+    if (self) {
         _baseURL = [url copy];
         _reachedLastPage = NO;
+        _networkService = [NetworkService sharedInstance];
+        
+        // Create operation queue with limited concurrency
+        _operationQueue = [[NSOperationQueue alloc] init];
+        _operationQueue.maxConcurrentOperationCount = 4;
     }
     return self;
 }
 
-#pragma mark - Abstract methods
+#pragma mark - Error Handling
 
-- (void)loadDetailsOfMod:(NSMutableDictionary *)item {
-    NSAssert(NO, @"Subclasses must override -loadDetailsOfMod:");
-    [self doesNotRecognizeSelector:_cmd];
+- (NSError *)errorWithCode:(ModpackAPIErrorCode)code message:(NSString *)message underlyingError:(NSError *)underlyingError {
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+    userInfo[NSLocalizedDescriptionKey] = message;
+    if (underlyingError) {
+        userInfo[NSUnderlyingErrorKey] = underlyingError;
+    }
+    return [NSError errorWithDomain:@"ModpackAPIErrorDomain" code:code userInfo:userInfo];
 }
 
-- (NSMutableArray *)searchModWithFilters:(NSDictionary *)searchFilters previousPageResult:(NSMutableArray *)prevResult {
-    NSAssert(NO, @"Subclasses must override -searchModWithFilters:previousPageResult:");
-    [self doesNotRecognizeSelector:_cmd];
-    return nil;
+#pragma mark - Network Requests
+
+- (NSString *)cacheKeyForEndpoint:(NSString *)endpoint params:(NSDictionary *)params {
+    NSData *paramsData = [NSJSONSerialization dataWithJSONObject:params ?: @{} options:0 error:nil];
+    NSString *paramsStr = paramsData ? [[NSString alloc] initWithData:paramsData encoding:NSUTF8StringEncoding] : @"";
+    return [NSString stringWithFormat:@"%@-%@", endpoint, paramsStr];
 }
 
-- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath {
-    NSAssert(NO, @"Subclasses must override -downloader:submitDownloadTasksFromPackage:toPath:");
-    [self doesNotRecognizeSelector:_cmd];
+- (void)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params completion:(void (^)(id, NSError *))completion {
+    if (!endpoint) {
+        if (completion) {
+            NSError *error = [self errorWithCode:ModpackAPIErrorCodeParsing 
+                                         message:@"Invalid endpoint" 
+                                 underlyingError:nil];
+            completion(nil, error);
+        }
+        return;
+    }
+    
+    NSString *url = [self.baseURL stringByAppendingPathComponent:endpoint];
+    NSString *cacheKey = [self cacheKeyForEndpoint:endpoint params:params];
+    
+    [self.networkService GET:url parameters:params headers:nil cacheName:cacheKey completion:^(id responseObject, NSError *error) {
+        if (error) {
+            self.lastError = error;
+        }
+        
+        if (completion) {
+            completion(responseObject, error);
+        }
+    }];
 }
-
-#pragma mark - Network requests
 
 - (id)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params {
     if (!endpoint) {
@@ -42,75 +80,288 @@
     
     __block id result = nil;
     __block NSError *requestError = nil;
-    dispatch_group_t group = dispatch_group_create();
-    dispatch_group_enter(group);
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     
-    NSString *url = [self.baseURL stringByAppendingPathComponent:endpoint];
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    
-    [manager GET:url parameters:params headers:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+    [self getEndpoint:endpoint params:params completion:^(id responseObject, NSError *error) {
         result = responseObject;
-        dispatch_group_leave(group);
-    } failure:^(NSURLSessionTask *operation, NSError *error) {
         requestError = error;
-        self.lastError = error;
-        dispatch_group_leave(group);
+        dispatch_semaphore_signal(semaphore);
     }];
     
-    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
     
     if (requestError) {
+        self.lastError = requestError;
         NSLog(@"[ModpackAPI] GET request to %@ failed: %@", endpoint, requestError);
     }
     
     return result;
 }
 
-- (void)getEndpoint:(NSString *)endpoint params:(NSDictionary *)params completion:(void (^)(id, NSError *))completion {
-    if (!endpoint) {
-        if (completion) {
-            NSError *error = [NSError errorWithDomain:@"ModpackAPIErrorDomain" 
-                                                 code:100 
-                                             userInfo:@{NSLocalizedDescriptionKey: @"Invalid endpoint"}];
-            completion(nil, error);
+#pragma mark - Extraction Methods
+
+- (void)extractArchive:(UZKArchive *)archive directory:(NSString *)dir toPath:(NSString *)path progress:(void (^)(double))progressCallback error:(NSError **)error {
+    // Count files for progress tracking
+    __block NSUInteger totalFiles = 0;
+    __block NSUInteger processedFiles = 0;
+    
+    NSError *countError = nil;
+    [archive performOnFilesInArchive:^(UZKFileInfo *fileInfo, BOOL *stop) {
+        if ([fileInfo.filename hasPrefix:dir]) {
+            totalFiles++;
+        }
+    } error:&countError];
+    
+    if (countError) {
+        if (error) {
+            *error = countError;
         }
         return;
     }
     
-    NSString *url = [self.baseURL stringByAppendingPathComponent:endpoint];
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    if (totalFiles == 0) {
+        if (progressCallback) {
+            progressCallback(1.0);
+        }
+        return;
+    }
     
-    [manager GET:url parameters:params headers:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        if (completion) {
-            completion(responseObject, nil);
+    // Extract with progress updates
+    NSError *archiveError = nil;
+    [archive performOnFilesInArchive:^(UZKFileInfo *fileInfo, BOOL *stop) {
+        if (![fileInfo.filename hasPrefix:dir]) {
+            return;
         }
-    } failure:^(NSURLSessionTask *operation, NSError *error) {
-        self.lastError = error;
-        NSLog(@"[ModpackAPI] Async GET request to %@ failed: %@", endpoint, error);
-        if (completion) {
-            completion(nil, error);
+        
+        NSString *relativePath = [fileInfo.filename substringFromIndex:dir.length];
+        if ([relativePath hasPrefix:@"/"]) {
+            relativePath = [relativePath substringFromIndex:1];
         }
-    }];
+        
+        if (relativePath.length == 0) {
+            processedFiles++;
+            if (progressCallback) {
+                progressCallback((double)processedFiles / totalFiles);
+            }
+            return;
+        }
+        
+        NSString *destPath = [path stringByAppendingPathComponent:relativePath];
+        NSString *destDir = fileInfo.isDirectory ? destPath : [destPath stringByDeletingLastPathComponent];
+        
+        NSError *dirError = nil;
+        if (![NSFileManager.defaultManager fileExistsAtPath:destDir]) {
+            [[NSFileManager defaultManager] createDirectoryAtPath:destDir
+                                     withIntermediateDirectories:YES
+                                                      attributes:nil
+                                                           error:&dirError];
+            if (dirError) {
+                *stop = YES;
+                if (error) {
+                    *error = dirError;
+                }
+                return;
+            }
+        }
+        
+        if (fileInfo.isDirectory) {
+            processedFiles++;
+            if (progressCallback) {
+                progressCallback((double)processedFiles / totalFiles);
+            }
+            return;
+        }
+        
+        NSError *extractError = nil;
+        NSData *fileData = [archive extractData:fileInfo error:&extractError];
+        if (extractError) {
+            *stop = YES;
+            if (error) {
+                *error = extractError;
+            }
+            return;
+        }
+        
+        NSError *writeError = nil;
+        BOOL written = [fileData writeToFile:destPath options:NSDataWritingAtomic error:&writeError];
+        if (!written) {
+            *stop = YES;
+            if (error) {
+                *error = writeError;
+            }
+            return;
+        }
+        
+        processedFiles++;
+        if (progressCallback) {
+            progressCallback((double)processedFiles / totalFiles);
+        }
+    } error:&archiveError];
+    
+    if (archiveError && error) {
+        *error = archiveError;
+    }
 }
 
-#pragma mark - Modpack installation
+#pragma mark - Template Methods
+
+- (NSDictionary *)processManifest:(NSDictionary *)manifest error:(NSError **)error {
+    // Default implementation - subclasses should override
+    return manifest;
+}
+
+- (NSString *)getManifestFilename {
+    // Default implementation - subclasses should override
+    return @"manifest.json";
+}
+
+- (NSArray *)getFilesFromManifest:(NSDictionary *)manifest {
+    // Default implementation - subclasses should override
+    return @[];
+}
+
+#pragma mark - API Interface Methods
+
+- (NSMutableArray *)searchModWithFilters:(NSDictionary *)searchFilters previousPageResult:(NSMutableArray *)prevResult {
+    // Subclasses must implement
+    [self doesNotRecognizeSelector:_cmd];
+    return nil;
+}
+
+- (void)loadDetailsOfMod:(NSMutableDictionary *)item completion:(void (^)(NSError *))completion {
+    // Subclasses must implement
+    [self doesNotRecognizeSelector:_cmd];
+}
 
 - (void)installModpackFromDetail:(NSDictionary *)modDetail atIndex:(NSUInteger)selectedVersion {
-    if (!modDetail) {
-        NSLog(@"[ModpackAPI] Cannot install modpack: nil modDetail");
-        return;
-    }
-    
+    // Default implementation that can be overridden
     NSDictionary *userInfo = @{
         @"detail": modDetail,
         @"index": @(selectedVersion)
     };
     
     dispatch_async(dispatch_get_main_queue(), ^{
-        [NSNotificationCenter.defaultCenter postNotificationName:@"InstallModpack" 
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"InstallModpack" 
                                                           object:self 
                                                         userInfo:userInfo];
     });
+}
+
+- (void)downloader:(MinecraftResourceDownloadTask *)downloader submitDownloadTasksFromPackage:(NSString *)packagePath toPath:(NSString *)destPath {
+    dispatch_queue_t extractionQueue = dispatch_queue_create("com.modpack.extraction", DISPATCH_QUEUE_CONCURRENT);
+    
+    // Add extraction task UI
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [downloader.fileList addObject:@"Extracting modpack index"];
+        NSProgress *indexProgress = [NSProgress progressWithTotalUnitCount:1];
+        indexProgress.kind = NSProgressKindFile;
+        [downloader.progressList addObject:indexProgress];
+        [downloader.progress addChild:indexProgress withPendingUnitCount:1];
+    });
+    
+    // Extract on background queue
+    dispatch_async(extractionQueue, ^{
+        NSError *error;
+        UZKArchive *archive = [[UZKArchive alloc] initWithPath:packagePath error:&error];
+        if (error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Failed to open modpack package: %@", error.localizedDescription]];
+            });
+            return;
+        }
+
+        // Extract and parse manifest
+        NSString *manifestFilename = [self getManifestFilename];
+        NSData *manifestData = [archive extractDataFromFile:manifestFilename error:&error];
+        if (!manifestData) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [downloader finishDownloadWithErrorString:@"Failed to extract modpack manifest"];
+            });
+            return;
+        }
+        
+        NSDictionary *manifest = [NSJSONSerialization JSONObjectWithData:manifestData options:0 error:&error];
+        if (error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [downloader finishDownloadWithErrorString:@"Failed to parse modpack manifest"];
+            });
+            return;
+        }
+
+        // Process manifest through template method
+        NSError *processError = nil;
+        NSDictionary *processedManifest = [self processManifest:manifest error:&processError];
+        
+        if (processError) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [downloader finishDownloadWithErrorString:[NSString stringWithFormat:@"Invalid modpack manifest: %@", processError.localizedDescription]];
+            });
+            return;
+        }
+
+        // Complete index extraction
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSProgress *indexProgress = [downloader.progressList lastObject];
+            indexProgress.completedUnitCount = 1;
+        });
+        
+        // Handle overrides extraction
+        [self extractOverrides:archive toPath:destPath withDownloader:downloader];
+        
+        // Process mod files
+        NSArray *files = [self getFilesFromManifest:processedManifest];
+        if (files.count > 0) {
+            [self downloadModFiles:files toPath:destPath withDownloader:downloader];
+        } else {
+            // Finalize installation even without mod files
+            [self finalizeInstallation:processedManifest toPath:destPath withDownloader:downloader];
+        }
+    });
+}
+
+- (void)extractOverrides:(UZKArchive *)archive toPath:(NSString *)destPath withDownloader:(MinecraftResourceDownloadTask *)downloader {
+    // This is a common implementation for extracting overrides
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [downloader.fileList addObject:@"Extracting modpack overrides"];
+        NSProgress *overridesProgress = [NSProgress progressWithTotalUnitCount:100];
+        overridesProgress.kind = NSProgressKindFile;
+        [downloader.progressList addObject:overridesProgress];
+        [downloader.progress addChild:overridesProgress withPendingUnitCount:100];
+    });
+    
+    NSError *extractError = nil;
+    
+    // Extract overrides
+    [self extractArchive:archive directory:@"overrides" toPath:destPath progress:^(double progress) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSProgress *overridesProgress = downloader.progressList.count >= 2 ? downloader.progressList[1] : nil;
+            if (overridesProgress) {
+                overridesProgress.completedUnitCount = (int64_t)(progress * 100);
+            }
+        });
+    } error:&extractError];
+    
+    // Optional extraction of client-specific files
+    if (!extractError) {
+        [ModpackUtils archive:archive extractDirectory:@"client-overrides" toPath:destPath error:nil];
+    }
+    
+    // Delete the package file to free up space
+    NSError *removeError = nil;
+    [[NSFileManager defaultManager] removeItemAtPath:packagePath error:&removeError];
+    if (removeError) {
+        NSLog(@"[ModpackAPI] Warning: Failed to delete modpack package: %@", removeError);
+    }
+}
+
+- (void)downloadModFiles:(NSArray *)files toPath:(NSString *)destPath withDownloader:(MinecraftResourceDownloadTask *)downloader {
+    // Subclasses should implement this
+    [self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)finalizeInstallation:(NSDictionary *)manifest toPath:(NSString *)destPath withDownloader:(MinecraftResourceDownloadTask *)downloader {
+    // Subclasses should implement this
+    [self doesNotRecognizeSelector:_cmd];
 }
 
 @end
