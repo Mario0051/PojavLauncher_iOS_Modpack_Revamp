120dc781a4a3c6ea7b524b34b7abc5bb5a9672c6
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-12 10:44:36 -0500

    More Detailed Download Reporting

--- Natives/DownloadProgressViewController.h
+++ Natives/DownloadProgressViewController.h
@@ -6,4 +6,26 @@
 
 - (instancetype)initWithTask:(MinecraftResourceDownloadTask *)task;
 
+/**
+ * Closes the view controller. If a download is in progress,
+ * shows a confirmation dialog before dismissing.
+ */
+- (void)actionClose;
+
+/**
+ * Removes all progress observers to prevent memory leaks
+ */
+- (void)removeAllProgressObservers;
+
+/**
+ * Removes a specific progress observer
+ * @param progress The progress object to stop observing
+ */
+- (void)removeProgressObserver:(NSProgress *)progress;
+
+/**
+ * Refreshes the UI with current progress information
+ */
+- (void)refreshProgressUI;
+
 @end
