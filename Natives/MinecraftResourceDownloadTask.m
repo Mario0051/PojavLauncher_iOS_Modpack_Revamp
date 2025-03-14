5008b6c8c6724d32a2aac3b996116e7919d4d237
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-14 16:46:23 -0500

    Update MinecraftResourceDownloadTask.m

--- Natives/MinecraftResourceDownloadTask.m
+++ Natives/MinecraftResourceDownloadTask.m
@@ -8,7 +8,7 @@
 #import "MinecraftResourceDownloadTask.h"
 #import "MinecraftResourceUtils.h"
 #import "ios_uikit_bridge.h"
-#import "PLProfiles.m"
+#import "PLProfiles.h"
 #import "utils.h"
 
 @interface MinecraftResourceDownloadTask ()
