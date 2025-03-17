f4bed3d38c6a75f721d8a46ffe7339ec348efd6b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-17 08:31:42 -0500

    Update ForgeInstallViewController.h

--- Natives/installer/ForgeInstallViewController.h
+++ Natives/installer/ForgeInstallViewController.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
-@interface ForgeInstallViewController : UITableViewController
+@interface ForgeInstallViewController : UITableViewController<UISearchResultsUpdating, UISearchBarDelegate>
 
 // To initialize with plain style
 - (instancetype)init;
