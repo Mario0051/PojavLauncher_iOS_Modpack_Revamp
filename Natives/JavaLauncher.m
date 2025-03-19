8206218ad84bf14d3efd270b8b2d3b0851c9797c
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-19 09:21:26 -0500

    Update JavaLauncher.m

--- Natives/JavaLauncher.m
+++ Natives/JavaLauncher.m
@@ -146,8 +146,8 @@ void init_loadDefaultEnv() {
     // Override OpenGL version to 4.1 for Zink
     setenv("MESA_GL_VERSION_OVERRIDE", "4.1", 1);
 
-    // Runs JVM in a separate thread - disabled to avoid conflicts with XstartOnFirstThread
-    // setenv("HACK_IGNORE_START_ON_FIRST_THREAD", "1", 1);
+    // Runs JVM in a separate thread - required for touch input to work
+    setenv("HACK_IGNORE_START_ON_FIRST_THREAD", "1", 1);
 }
 
 void init_loadCustomEnv() {
