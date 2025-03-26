7c88306051025e188fccae2a10d048e80c479161
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-26 18:53:35 -0500

    Update ModpackInstallViewController.h

--- Natives/installer/ModpackInstallViewController.h
+++ Natives/installer/ModpackInstallViewController.h
@@ -1,6 +1,22 @@
 #import <UIKit/UIKit.h>
 
-@interface ModpackInstallViewController : UITableViewController<UISearchResultsUpdating>
+@interface ModpackInstallViewController : UITableViewController<UISearchResultsUpdating, UIContextMenuInteractionDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 - (void)appendToUnifiedSearchResults:(NSArray *)newResults;
 - (void)updateSearchAndRefreshUI;
+
+// Setup methods
+- (void)setupTagCollectionView;
+- (void)setupSegmentedControl;
+- (void)setupSearchController;
+- (void)setupRefreshControl;
+- (void)setupEmptyStateView;
+
+// UI state management methods
+- (void)switchToLoadingState;
+- (void)switchToReadyState;
+
+// Data loading methods
+- (void)updateSearchResults;
+- (void)loadSearchResultsWithPrevList:(BOOL)prevList;
+- (void)loadMoreResults;
 @end
