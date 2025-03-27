ba6f37ca6cc27e0ab554c5f81095ea2e2d448a26
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-26 20:10:37 -0500

    Update ModpackUtils.h

--- Natives/installer/modpack/ModpackUtils.h
+++ Natives/installer/modpack/ModpackUtils.h
@@ -3,7 +3,12 @@
 
 @interface ModpackUtils : NSObject
 
+// Archive extraction methods
 + (void)archive:(UZKArchive *)archive extractDirectory:(NSString *)dir toPath:(NSString *)path error:(NSError **)error;
 + (NSDictionary *)infoForDependencies:(NSDictionary *)dependency;
+
+// Dictionary safety methods
++ (void)safeSetObject:(id)object forKey:(id<NSCopying>)key inDictionary:(NSMutableDictionary *)dict;
++ (NSMutableDictionary *)safeMutableDictionaryWithDictionary:(NSDictionary *)dict;
 
 @end
