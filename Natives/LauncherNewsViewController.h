2bf1bf5ff04786efa2d248d8abbcbfa1124a70b5
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-31 09:44:24 -0500

    Update LauncherNewsViewController.h

--- Natives/LauncherNewsViewController.h
+++ Natives/LauncherNewsViewController.h
@@ -1,5 +1,15 @@
 #import <UIKit/UIKit.h>
 
-@interface LauncherNewsViewController : UIViewController
+NSMutableArray<NSDictionary *> *localVersionList, *remoteVersionList;
+
+@interface LauncherNavigationController : UINavigationController
+
+@property(nonatomic) UIProgressView *progressViewMain, *progressViewSub;
+@property(nonatomic) UILabel* progressText;
+@property(nonatomic) UIButton* buttonInstall;
+
+- (void)enterModInstallerWithPath:(NSString *)path hitEnterAfterWindowShown:(BOOL)hitEnter;
+- (void)fetchLocalVersionList;
+- (void)setInteractionEnabled:(BOOL)enable forDownloading:(BOOL)downloading;
 
 @end
