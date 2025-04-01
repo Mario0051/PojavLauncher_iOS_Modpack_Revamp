56ab572ce19f8756a30df90a9fdd02f88fdbaf4b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-04-01 11:14:14 -0500

    Update ModrinthAPI.h

--- Natives/installer/modpack/ModrinthAPI.h
+++ Natives/installer/modpack/ModrinthAPI.h
@@ -6,4 +6,15 @@
 @property(nonatomic, strong) NSString *userAgent;
 @property(nonatomic, strong) NSURLSession *session;
 
+/**
+ * Checks if the modpack requires Forge or NeoForge and handles installation if needed
+ *
+ * @param downloader The download task managing the modpack installation
+ * @param dependencies The dependencies dictionary from the modpack index
+ * @param profileName The name of the profile being created
+ */
+- (void)checkAndInstallForge:(MinecraftResourceDownloadTask *)downloader 
+            withDependencies:(NSDictionary *)dependencies 
+                 profileName:(NSString *)profileName;
+
 @end
